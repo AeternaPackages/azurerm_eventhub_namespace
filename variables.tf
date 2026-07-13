2fdb1e06754b8d614264cfb8e03a073d37a68272
Daniel Allen <dan@aeternagroup.net>
2026-07-13 19:41:01 +0100

    Update to schema version v4.80.0

--- variables.tf
+++ variables.tf
@@ -30,34 +30,34 @@ EOT
     name                          = string
     resource_group_name           = string
     sku                           = string
-    auto_inflate_enabled          = optional(bool)   # Default: false
-    capacity                      = optional(number) # Default: 1
+    auto_inflate_enabled          = optional(bool)
+    capacity                      = optional(number)
     dedicated_cluster_id          = optional(string)
-    local_authentication_enabled  = optional(bool) # Default: true
+    local_authentication_enabled  = optional(bool)
     maximum_throughput_units      = optional(number)
-    minimum_tls_version           = optional(string) # Default: "1.2"
-    public_network_access_enabled = optional(bool)   # Default: true
+    minimum_tls_version           = optional(string)
+    public_network_access_enabled = optional(bool)
     tags                          = optional(map(string))
     identity = optional(object({
       identity_ids = optional(set(string))
       type         = string
     }))
-    network_rulesets = optional(object({
-      default_action = string
+    network_rulesets = optional(list(object({
+      default_action = optional(string)
       ip_rule = optional(list(object({
-        action  = optional(string) # Default: "Allow"
-        ip_mask = string
+        action  = optional(string)
+        ip_mask = optional(string)
       })))
-      public_network_access_enabled  = optional(bool) # Default: true
+      public_network_access_enabled  = optional(bool)
       trusted_service_access_enabled = optional(bool)
       virtual_network_rule = optional(list(object({
         ignore_missing_virtual_network_service_endpoint = optional(bool)
-        subnet_id                                       = string
+        subnet_id                                       = optional(string)
       })))
-    }))
+    })))
     eventhub_namespace_customer_managed_keys = optional(map(object({
       key_vault_key_ids                 = set(string)
-      infrastructure_encryption_enabled = optional(bool) # Default: false
+      infrastructure_encryption_enabled = optional(bool)
       user_assigned_identity_id         = optional(string)
     })))
   }))
